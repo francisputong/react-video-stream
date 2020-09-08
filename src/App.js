e30a719fef652458ba10dbe6f6bef95cef6c3b68
francisputong <francisputong01@gmail.com>
2020-09-08 21:50:33 +0800

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
-import SearchBar from "./components/SearchBar";
+import Navbar from "./components/Navbar";
+import VideoPopular from "./components/Navigation/VideoPopular";
+import Projects from "./components/Navigation/Projects";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Route path="/" exact component={VideoPopular} />
+        <Route path="/projects" exact component={Projects} />
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
